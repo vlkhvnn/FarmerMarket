7fb4c5fa3461fffb22b99d60c6c3580775362cbe
vlkhvnn <tanir.alikh@gmail.com>
2024-11-19 16:47:32 +0500

    init

---
+++ Farmers-Market/Farmers-Market/Farmers_MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Farmers_MarketApp.swift
+//  Farmers-Market
+//
+//  Created by Alikhan Tangirbergen on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Farmers_MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Farmers-Market/Farmers-Market/Views/MainView.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+struct MainView: View {
+    @State private var selection = 0
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+                .tag(0)
+            
+            CategoriesView()
+                .tabItem {
+                    Label("Categories", systemImage: "magnifyingglass")
+                }
+                .tag(1)
+            
+            FavoritesView()
+                .tabItem {
+                    Label("Favorites", systemImage: "heart.fill")
+                }
+                .tag(2)
+
+            OrdersView()
+                .tabItem {
+                    Label("Orders", systemImage: "bag.fill")
+                }
+                .tag(3)
+
+            AccountView()
+                .tabItem {
+                    Label("Account", systemImage: "person.fill")
+                }
+                .tag(2)
+        }
+    }
+}
